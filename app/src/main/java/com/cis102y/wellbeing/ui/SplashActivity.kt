c6526350ac82e6b01a9e6c62fc3b7d46d6697787
mdadi1130 <mdadi22@gmail.com>
2020-10-05 06:24:00 -0400

    Created Splash Screen

---
+++ app/src/main/java/com/cis102y/wellbeing/ui/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.cis102y.wellbeing.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cis102y.wellbeing.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
