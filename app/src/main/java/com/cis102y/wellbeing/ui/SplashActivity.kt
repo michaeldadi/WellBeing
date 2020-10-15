3602427dcb737f23202e38efdc89c449f6f15401
mdadi1130 <mdadi22@gmail.com>
2020-10-14 20:24:14 -0400

    Splash Screen code component Added iml files and modules

--- app/src/main/java/com/cis102y/wellbeing/ui/SplashActivity.kt
+++ app/src/main/java/com/cis102y/wellbeing/ui/SplashActivity.kt
@@ -1,14 +1,23 @@
 package com.cis102y.wellbeing.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.cis102y.wellbeing.MainActivity
 import com.cis102y.wellbeing.R
 
 class SplashActivity : AppCompatActivity() {
 
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        //Delaying 3 seconds to start MainActivity
+        handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
